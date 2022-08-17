5241aafd312ef9985b8f9f8f748f4ce441a1e4df
Titania01 <adegbitebidmus@gmail.com>
2022-08-17 03:43:53 +0100

    update

--- organisms/AboutSection.tsx
+++ organisms/AboutSection.tsx
@@ -1,9 +1,38 @@
 import Image from "next/image";
 
+const languages = [
+  {
+    name: "HTML",
+  },
+  {
+    name: "CSS",
+  },
+  {
+    name: "JAVASCRIPT",
+  },
+  {
+    name: "TYPESCRIPT",
+  },
+];
+const techStack = [
+  {
+    name: "REACT",
+  },
+  {
+    name: "NODE",
+  },
+  {
+    name: "EXPRESS",
+  },
+  {
+    name: "REDUX",
+  },
+];
+
 const AboutSection = ({ about = "" }) => {
   return (
-    <div className="second-bg text-center" id="about">
-      <div className="lg:pl-[33.1875rem] pt-[16.75rem] dark:bg-[#1F2937E5] dark:bg-none">
+    <div className="second-bg flex justify-center" id="about">
+      <div className=" pt-[16.75rem] dark:bg-[#1F2937E5] dark:bg-none">
         <div>
           <p className="dark:text-white">ABOUT ME</p>
           <p className="font-bold text-[2.5rem] text-[#1f2037] dark:text-white">
@@ -15,7 +44,7 @@ const AboutSection = ({ about = "" }) => {
               : "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Imperdiet nunc ultrices ac mauris, quis erat consequat odio. Pretium aliquet nunc eget consectetur diam felis justo. Tempor ornare   nunc facilisi amet. Sed dui orci, ut pharetra. Vulputate id lectus laoreet sagittis non. Pharetra purus quam mauris malesuada etiam nullam. Ultrices nec in massa consequat erat iaculis leo."}
           </p>
 
-          <div className="lg:grid lg:grid-cols-4 mt-12 dark:text-white px-4">
+          {/* <div className="lg:grid lg:grid-cols-4 mt-12 dark:text-white px-4">
             <div className="mb-8">
               <h2 className="text-[#1f2937] dark:text-white font-bold text-[1.5rem]">
                 Languages
@@ -82,7 +111,7 @@ const AboutSection = ({ about = "" }) => {
                 </div>
               </div>
             </div>
-          </div>
+          </div> */}
         </div>
       </div>
     </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,18 @@ module.exports = {
   ],
   darkMode: "class",
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: (theme) => ({
+        intro: "url('../../public/images/bg1.png')",
+        intro: "url('../../public/images/bg2.png')",
+        intro: "url('../../public/images/bg3.png')",
+        intro: "url('../../public/images/bg4.png')",
+        "dark-intro": "url('../../Public/darkBg1.png')",
+        "dark-intro": "url('../../Public/darkBg2.png')",
+        "dark-intro": "url('../../Public/darkBg3.png')",
+        "dark-intro": "url('../../Public/darkBg4.png')",
+      }),
+    },
   },
   plugins: [],
 };
